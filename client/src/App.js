fb7a6e17b2091f7c7a23449d7f09db8af3582037
matthausen <matteo.fusilli@hotmail.it>
2020-12-27 12:22:08 +0100

    new frontend build google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-import React, { useEffect } from 'react';
-import ReactGA from "react-ga";
+import React from 'react';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize('UA-185972921-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  })
   return (
     <div className="App">
       <SearchBar />
