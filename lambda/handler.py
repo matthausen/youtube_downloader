4785e041bf378f7bb509b592eaa32cc6e875d05c
matthausen <matteo.fusilli@hotmail.it>
2021-02-02 20:11:44 +0000

    upload files as .mp3 to s3 bucket

--- lambda/handler.py
+++ lambda/handler.py
@@ -2,7 +2,8 @@ import json, os, re, glob
 from youtube_search import YoutubeSearch
 import pafy
 import boto3
-from utils import upload_file, extract_audio
+import moviepy.editor as mp
+from utils import upload_file
 
 def get_tracks(event, context):
 
@@ -19,11 +20,13 @@ def get_tracks(event, context):
 def download_tracks(event, _):
   body = event["body"]
   songs = eval(body)
-
   TMP_FOLDER = '/tmp'
 
-  # aws lambda allows to write only in /tmp folder
+  # aws lambda allows to write only in /tmp/tracks folder
   os.chdir(TMP_FOLDER)
+  new_dir = os.path.join(TMP_FOLDER, 'tracks')
+  os.mkdir(new_dir)
+  os.chdir(new_dir)
    
    # Download all tracks videos as .mp4
   for song in songs:
@@ -31,25 +34,25 @@ def download_tracks(event, _):
     pafy.new(url).getbest().download()
   
   # Upload to s3 
+  for file in glob.glob("*.mp4"):
+    mp4_path = os.path.join(os.getcwd(),file)
+    print(mp4_path)
+    mp3_path = os.path.join(os.getcwd(),os.path.splitext(file)[0]+'.mp3')
+    print(mp3_path)
+    new_file = mp.AudioFileClip(mp4_path)
+    new_file.write_audiofile(mp3_path)  
+    os.remove(mp4_path)
+
+  # Upload .mp3 files to S3 bucket
   s3 = boto3.client('s3')
-  for file in glob.glob("*.mp4"):  
+  for file in glob.glob("*.mp3"):
+    print(f'Uploading {file} to the bucket ...')
     with open(file, "rb") as f:
       s3.upload_fileobj(f, "whitechapel-dev-tracks", file)
 
   response = {
     "statusCode": 200,
     "body": json.dumps(songs)
-  }
-
-  return response
-
-def download_zip(event, _):
-  
-  extract_audio()
-  
-  response = {
-    "statusCode": 200,
-    "body": "Downloaded .zip"
   }
 
   return response
--- lambda/utils.py
+++ lambda/utils.py
@@ -3,8 +3,6 @@ from botocore.exceptions import ClientError
 import os, time
 import moviepy.editor as mp
 import shutil
-from urllib.parse import urlparse
-
 
 def upload_file(file_name, bucket, object_name=None):
   
@@ -18,22 +16,3 @@ def upload_file(file_name, bucket, object_name=None):
       logging.error(e)
       return False
   return True
-
-def extract_audio():
-  s3 = boto3.resource('s3')
-  bucket = s3.Bucket('whitechapel-dev-tracks')
-
-  file_path = 's3://whitechapel-dev-tracks'
-
-  # Convert all videos to mp3s and create a .zip file
-  for obj in bucket.objects.all():
-    if obj.key.endswith('.mp4'):
-      file = obj.key
-      mp4_path = os.path.join(file_path,file)
-      print('mp4_path: ',mp4_path)
-      mp3_path = os.path.join(file_path,os.path.splitext(file)[0]+'.mp3')
-      print('mp3_path: ',mp3_path)
-      new_file = mp.AudioFileClip(mp4_path)
-      new_file.write_audiofile(mp3_path)  
-      os.remove(mp4_path)
-      shutil.make_archive('Music ' + str(time.time()), 'zip', file_path.path)
